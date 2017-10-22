8455e168f893c9dc44570978a03d80cdb9b230c0
Yang Liu <>
2017-10-22 14:15:52 -0400

    complete code, works for 20m/s

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include "MPC.h"
 #include "json.hpp"
 
+const double L = 2.67;
 // for convenience
 using json = nlohmann::json;
 
@@ -20,22 +21,28 @@ double rad2deg(double x) { return x * 180 / pi(); }
 // Checks if the SocketIO event has JSON data.
 // If there is data the JSON object in string format will be returned,
 // else the empty string "" will be returned.
-string hasData(string s) {
+string hasData(string s)
+{
   auto found_null = s.find("null");
   auto b1 = s.find_first_of("[");
   auto b2 = s.rfind("}]");
-  if (found_null != string::npos) {
+  if (found_null != string::npos)
+  {
     return "";
-  } else if (b1 != string::npos && b2 != string::npos) {
+  }
+  else if (b1 != string::npos && b2 != string::npos)
+  {
     return s.substr(b1, b2 - b1 + 2);
   }
   return "";
 }
 
 // Evaluate a polynomial.
-double polyeval(Eigen::VectorXd coeffs, double x) {
+double polyeval(Eigen::VectorXd coeffs, double x)
+{
   double result = 0.0;
-  for (int i = 0; i < coeffs.size(); i++) {
+  for (int i = 0; i < coeffs.size(); i++)
+  {
     result += coeffs[i] * pow(x, i);
   }
   return result;
@@ -45,17 +52,21 @@ double polyeval(Eigen::VectorXd coeffs, double x) {
 // Adapted from
 // https://github.com/JuliaMath/Polynomials.jl/blob/master/src/Polynomials.jl#L676-L716
 Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
-                        int order) {
+                        int order)
+{
   assert(xvals.size() == yvals.size());
   assert(order >= 1 && order <= xvals.size() - 1);
   Eigen::MatrixXd A(xvals.size(), order + 1);
 
-  for (int i = 0; i < xvals.size(); i++) {
+  for (int i = 0; i < xvals.size(); i++)
+  {
     A(i, 0) = 1.0;
   }
 
-  for (int j = 0; j < xvals.size(); j++) {
-    for (int i = 0; i < order; i++) {
+  for (int j = 0; j < xvals.size(); j++)
+  {
+    for (int i = 0; i < order; i++)
+    {
       A(j, i + 1) = A(j, i) * xvals(j);
     }
   }
@@ -65,9 +76,25 @@ Eigen::VectorXd polyfit(Eigen::VectorXd xvals, Eigen::VectorXd yvals,
   return result;
 }
 
-int main() {
-  uWS::Hub h;
+void world2car(std::vector<double> ptsx, std::vector<double> ptsy, Eigen::VectorXd &ptsxc, Eigen::VectorXd &ptsyc,
+               double x, double y, double phi)
+{
+  double xc, yc;
+  double xm, ym;
+  for (int i = 0; i < ptsx.size(); i++)
+  {
+    xm = ptsx[i];
+    ym = ptsy[i];
+    xc = (xm - x) * cos(phi) + (ym - y) * sin(phi);
+    yc = -(xm - x) * sin(phi) + (ym - y) * cos(phi);
+    ptsxc[i] = xc;
+    ptsyc[i] = yc;
+  }
+}
 
+int main()
+{
+  uWS::Hub h;
   // MPC is initialized here!
   MPC mpc;
 
@@ -78,12 +105,15 @@ int main() {
     // The 2 signifies a websocket event
     string sdata = string(data).substr(0, length);
     cout << sdata << endl;
-    if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2') {
+    if (sdata.size() > 2 && sdata[0] == '4' && sdata[1] == '2')
+    {
       string s = hasData(sdata);
-      if (s != "") {
+      if (s != "")
+      {
         auto j = json::parse(s);
         string event = j[0].get<string>();
-        if (event == "telemetry") {
+        if (event == "telemetry")
+        {
           // j[1] is the data JSON object
           vector<double> ptsx = j[1]["ptsx"];
           vector<double> ptsy = j[1]["ptsy"];
@@ -91,15 +121,43 @@ int main() {
           double py = j[1]["y"];
           double psi = j[1]["psi"];
           double v = j[1]["speed"];
+          v = v * 0.44704;
+          double steer = j[1]["steering_angle"];
+          double throttle = j[1]["throttle"];
+          throttle = throttle*2.23;
 
+
+          Eigen::VectorXd ptsxc(ptsx.size());
+          Eigen::VectorXd ptsyc(ptsy.size());
+          world2car(ptsx, ptsy, ptsxc, ptsyc, px, py, psi);
+          auto coeffs = polyfit(ptsxc, ptsyc, 3);
+          // double cte = polyeval(coeffs, 0);
+          double cte = coeffs[0];
+          double epsi = -atan(coeffs[1]);
+          Eigen::VectorXd state(6);
+
+          double dt = 0.1;
+          double x_act = v * dt;
+          double y_act = 0;
+          double psi_act = -v / L * steer * dt;
+          double v_act = v + throttle * dt;
+          double cte_act = cte + v * sin(epsi) * dt;
+          double epsi_act = epsi - v / L * steer * dt;
+          state << x_act, y_act, psi_act, v_act, cte_act, epsi_act;
+
+          vector<double> mpc_x_vals;
+          vector<double> mpc_y_vals;
+
+          auto vars = mpc.Solve(state, coeffs, mpc_x_vals, mpc_y_vals);
           /*
           * TODO: Calculate steering angle and throttle using MPC.
           *
           * Both are in between [-1, 1].
           *
           */
-          double steer_value;
-          double throttle_value;
+          double steer_value = vars[0] / deg2rad(25);
+          std::cout<<"steer value: "<<steer_value<<'\n';
+          double throttle_value = vars[1]*0.447;
 
           json msgJson;
           // NOTE: Remember to divide by deg2rad(25) before you send the steering value back.
@@ -107,12 +165,14 @@ int main() {
           msgJson["steering_angle"] = steer_value;
           msgJson["throttle"] = throttle_value;
 
-          //Display the MPC predicted trajectory 
-          vector<double> mpc_x_vals;
-          vector<double> mpc_y_vals;
+          //Display the MPC predicted trajectory
+          // vector<double> mpc_x_vals;
+          // vector<double> mpc_y_vals;
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Green line
+
+          // world2car(ptsx, ptsy, mpc_x_vals, mpc_y_vals,px, py, psi);
 
           msgJson["mpc_x"] = mpc_x_vals;
           msgJson["mpc_y"] = mpc_y_vals;
@@ -120,13 +180,19 @@ int main() {
           //Display the waypoints/reference line
           vector<double> next_x_vals;
           vector<double> next_y_vals;
+          for (int i = 0; i < 10; i++)
+          {
+            double xx = 10 * i;
+            double yy = polyeval(coeffs, xx);
+            next_x_vals.push_back(xx);
+            next_y_vals.push_back(yy);
+          }
 
           //.. add (x,y) points to list here, points are in reference to the vehicle's coordinate system
           // the points in the simulator are connected by a Yellow line
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
-
 
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
@@ -142,7 +208,9 @@ int main() {
           this_thread::sleep_for(chrono::milliseconds(100));
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }
-      } else {
+      }
+      else
+      {
         // Manual driving
         std::string msg = "42[\"manual\",{}]";
         ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
@@ -156,9 +224,12 @@ int main() {
   h.onHttpRequest([](uWS::HttpResponse *res, uWS::HttpRequest req, char *data,
                      size_t, size_t) {
     const std::string s = "<h1>Hello world!</h1>";
-    if (req.getUrl().valueLength == 1) {
+    if (req.getUrl().valueLength == 1)
+    {
       res->end(s.data(), s.length());
-    } else {
+    }
+    else
+    {
       // i guess this should be done more gracefully?
       res->end(nullptr, 0);
     }
@@ -175,9 +246,12 @@ int main() {
   });
 
   int port = 4567;
-  if (h.listen(port)) {
+  if (h.listen(port))
+  {
     std::cout << "Listening to port " << port << std::endl;
-  } else {
+  }
+  else
+  {
     std::cerr << "Failed to listen to port" << std::endl;
     return -1;
   }
